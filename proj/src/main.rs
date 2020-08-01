27e26c71616e0d3518a166b081d045a634740559
sunakan <sunakan03@gmail.com>
2020-08-01 04:48:37 +0000

    proj

--- proj/src/main.rs
+++ proj/src/main.rs
@@ -1,3 +1,18 @@
+use pnet::datalink::{self, NetworkInterface};
+use std::env;
+
+#[derive(Clone, Debug)]
+struct PacketWithInterface {
+    interface: NetworkInterface,
+    packet: Vec<u8>,
+}
+
 fn main() {
+    let network_interface_name = env::args().nth(1).unwrap();
     println!("Hello, world!");
+    let interface = datalink::interfaces()
+        .into_iter()
+        .find(|iface| iface.name == network_interface_name)
+        .expect("入力されたネットワークインターフェースが見つかりませんでした");
+    println!("-----> {:?}", interface);
 }
